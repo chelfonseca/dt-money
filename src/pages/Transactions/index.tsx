16747fb6797527a8db56c3548347c14bfd0700e9
Michel Fonseca <michelbeserradafonseca@gmail.com>
2023-02-14 13:21:20 -0300

    Summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../comnponents/Header";
+import { Summary } from "../../comnponents/Summary";
 
 export function Transactions() {
     return (
         <div>
             <Header/>
+            <Summary/>
         </div>
     )
 }
