e120515edaa13d2a628cab9306eb12fc6c932369
Michel Fonseca <michelbeserradafonseca@gmail.com>
2023-02-14 13:52:08 -0300

    Transactions component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../comnponents/Header";
 import { Summary } from "../../comnponents/Summary";
+import { PriceHighLight, TransactionContainer, TransactionTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header/>
             <Summary/>
+            <TransactionContainer>
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de software</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburger</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                R$ 59,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionTable>
+            </TransactionContainer>
+
         </div>
     )
 }
