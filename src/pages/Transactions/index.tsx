8245c18e70ccdb7422daf62af077f564c1ff1f1e
Michel Fonseca <michelbeserradafonseca@gmail.com>
2023-02-16 15:58:22 -0300

    Transactions context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,34 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionContainer, TransactionTable } from "./styles";
 
-interface Transaction {
-    id: number;
-    description: string;
-    type: 'income' | 'outcome';
-    price: number;
-    category: string;
-    createAt: string;
-}
+
 
 export function Transactions() {
 
-    const [transaction, setTransactions] = useState<Transaction[]>([])
-
-    async function loadTransactions() {
-        const response = await fetch('http://localhost:3333/transactions')
-        const data =  await response.json()
-
-        setTransactions(data)
-   }
-
-    useEffect(() => {
-       
-       loadTransactions()
-    }
-    ,[])
+    const {transactions } = useContext(TransactionsContext)
 
     return (
         <div>
@@ -39,7 +20,7 @@ export function Transactions() {
                 <TransactionTable>
                     <tbody>
                         {
-                            transaction.map(transaction => {
+                            transactions.map(transaction => {
                                 return (
                                     <tr key={transaction.id}>
                                         <td width="50%">{transaction.description}</td>
